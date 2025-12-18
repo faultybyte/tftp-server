329583fa62568429bc429e9322e08af8fbb91411
Johan Emmanuel <johanemman243@gmail.com>
2025-12-18 23:31:55 +0530

    Implement ACK packet serialization

---
+++ src/AckPacket.cpp
@@ -0,0 +1,25 @@
+#include "AckPacket.hpp"
+#include <arpa/inet.h>
+
+AckPacket::AckPacket(uint16_t blockNumber) : blockNumber(blockNumber) {}
+
+Opcode AckPacket::getOpcode() const {
+    return Opcode::ACK;
+}
+
+std::vector<uint8_t> AckPacket::serialize() const {
+    std::vector<uint8_t> buffer;
+
+    uint16_t op = static_cast<uint16_t>(Opcode::ACK);
+    
+    buffer.push_back(op >> 8);
+    buffer.push_back(op & 0xff);
+    buffer.push_back(blockNumber >> 8);
+    buffer.push_back(blockNumber & 0xff);
+
+    return buffer;
+}
+
+uint16_t AckPacket::getBlockNumber() const {
+    return blockNumber;
+}
