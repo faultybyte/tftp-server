81c866e169e05a4241da4fe996c43adf0e2a1c87
Johan Emmanuel <johanemman243@gmail.com>
2025-12-20 00:06:20 +0530

    Implement DATA packet parsing

--- src/PacketParser.cpp
+++ src/PacketParser.cpp
@@ -1,5 +1,6 @@
 #include "PacketParser.hpp"
 #include "AckPacket.hpp"
+#include "DataPacket.hpp"
 #include <stdexcept>
 
 static std::unique_ptr<Packet> parseAckPacket(const std::vector<uint8_t>& buffer) {
@@ -11,6 +12,16 @@ static std::unique_ptr<Packet> parseAckPacket(const std::vector<uint8_t>& buffer
     return std::make_unique<AckPacket>(blockNumber);
 }
 
+static std::unique_ptr<Packet> parseDataPacket(const std::vector<uint8_t>& buffer) {
+    if (buffer.size() < 4)
+        throw std::runtime_error("Invalid DATA packet");
+
+    uint16_t blockNumber = (buffer[2] << 8) | buffer[3];
+    std::vector<uint8_t> data(buffer.begin() + 4, buffer.end());
+    
+    return std::make_unique<DataPacket>(blockNumber, data);
+}
+
 std::unique_ptr<Packet> PacketParser::parse(const std::vector<uint8_t>& buffer) {
     if (buffer.size() < 2)
         throw std::runtime_error("Invalid packet size");
@@ -18,6 +29,8 @@ std::unique_ptr<Packet> PacketParser::parse(const std::vector<uint8_t>& buffer) 
     uint16_t op = (buffer[0] << 8) | buffer[1];
 
     switch (op) {
+        case 3:
+            return parseDataPacket(buffer);
         case 4:
             return parseAckPacket(buffer);
         default:
