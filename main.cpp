269e6fc078ebd6725ae595004604f0fd8d2d2c0a
Johan Emmanuel <johanemman243@gmail.com>
2025-12-20 23:28:44 +0530

    Get the server running

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <Server.hpp>
 
 int main(int argc, char* argv[]) {
-    
+    uint16_t port = 6969;
+
+    if (argc > 1) {
+        port = static_cast<uint16_t>(std::stoi(argv[1]));
+    }
+
+    try {
+        TftpServer server(port);
+        server.start();
+    } catch (const std::exception& err) {
+        std::cerr << "Fatal Server Error: " << err.what() << '\n';
+        return 1;
+    }
+
     return 0;
 }
